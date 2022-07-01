d4c23599e722ee012e0c41f24a37eee343a6e3d7
Zodiac007 <harsh.dwivedi@scoopwhoop.com>
2022-07-01 22:27:47 +0530

    initial commit

---
+++ components/Modal.js
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+
+export default function Modal({ user, setIsModalOpen, setList }) {
+  const [localUser, setLocalUser] = useState({});
+
+  useEffect(() => {
+    setLocalUser(user);
+  }, [user]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setLocalUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    const listUsers = JSON.parse(localStorage.getItem("userData"));
+    let updatedList = listUsers;
+    if (localUser.id) {
+      updatedList = updatedList.map((user) => {
+        if (user.id === localUser.id) {
+          return localUser;
+        }
+        return user;
+      });
+    } else {
+      localUser.id = Math.random() * 10;
+      updatedList.push(localUser);
+    }
+    localStorage.setItem("userData", JSON.stringify(updatedList));
+    setList(updatedList);
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div
+      className="modal fade fixed top-0 left-0 w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+      id="exampleModalCenteredScrollable"
+      //   tabIndex="-1"
+      aria-labelledby="exampleModalCenteredScrollable"
+      aria-modal="true"
+      role="dialog"
+    >
+      <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable relative w-auto pointer-events-none">
+        <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+          <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+            <h5
+              className="text-xl font-medium leading-normal text-gray-800"
+              id="exampleModalCenteredScrollableLabel"
+            >
+              {user.login ? user.login : "Add User"}
+            </h5>
+          </div>
+          <div className="modal-body relative p-4">
+            <div className="flex flex-wrap -mx-3 mb-2">
+              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <input
+                  type="text"
+                  name="login"
+                  value={localUser.login}
+                  onChange={handleChange}
+                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Username"
+                />
+              </div>
+
+              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <input
+                  type="text"
+                  name="html_url"
+                  value={localUser.html_url}
+                  onChange={handleChange}
+                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Profile URL"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+            <button
+              type="button"
+              className="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+              data-bs-dismiss="modal"
+              onClick={() => setIsModalOpen(false)}
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1"
+            >
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/dashboard.js
@@ -0,0 +1,183 @@
+import { useEffect, useState } from "react";
+import Modal from "../components/Modal";
+
+export default function Dashboard({ data }) {
+  const [list, setList] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState("");
+  useEffect(() => {
+    if (data.length) {
+      localStorage.setItem("userData", JSON.stringify(data));
+      setList(data);
+    }
+  }, [data]);
+
+  const deleteUser = (id) => {
+    const listUsers = JSON.parse(localStorage.getItem("userData"));
+    const updatedList = listUsers.filter((user) => user.id !== id);
+    localStorage.setItem("userData", JSON.stringify(updatedList));
+    setList(updatedList);
+  };
+
+  const addUser = () => {};
+
+  return (
+    <div className="max-w-screen-lg mx-auto my-12">
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="bg-white border-b">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Avatar
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Username
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Profile Url
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      <button
+                        type="button"
+                        className="flex items-center px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                        data-bs-toggle="modal"
+                        onClick={() => {
+                          setIsModalOpen(true);
+                        }}
+                        data-bs-target="#exampleModalCenteredScrollable"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          width="16"
+                          height="16"
+                        >
+                          <path fill="none" d="M0 0h24v24H0z" />
+                          <path
+                            d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm6 6v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z"
+                            fill="rgba(255,255,255,1)"
+                          />
+                        </svg>
+                        <span className="pl-2"> Add User</span>
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {list.map((user, index) => (
+                    <tr className="bg-gray-100 border-b" key={user.id}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        <img
+                          src={
+                            user.avatar_url
+                              ? user.avatar_url
+                              : "https://avatars.githubusercontent.com/u/44?v=4"
+                          }
+                          className="rounded-full w-8"
+                          alt={`Avatar ${user.id}`}
+                        />
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {user.login}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <a
+                          href={user.html_url}
+                          rel="noreferrer"
+                          target="_blank"
+                          className="text-blue-900"
+                        >
+                          {user.html_url}
+                        </a>
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <div className="flex space-x-2 justify-start">
+                          <div>
+                            <button
+                              type="button"
+                              className="inline-block mr-4 rounded p-2 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                              onClick={() => deleteUser(user.id)}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="18"
+                                height="18"
+                              >
+                                <path fill="none" d="M0 0h24v24H0z" />
+                                <path
+                                  d="M4 8h16v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8zm2 2v10h12V10H6zm3 2h2v6H9v-6zm4 0h2v6h-2v-6zM7 5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v2h5v2H2V5h5zm2-1v1h6V4H9z"
+                                  fill="rgba(255,255,255,1)"
+                                />
+                              </svg>
+                            </button>
+                            <button
+                              type="button"
+                              className="inline-block rounded-full p-2 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                              data-bs-toggle="modal"
+                              onClick={() => {
+                                setIsModalOpen(true);
+                                setSelectedUser(user);
+                              }}
+                              data-bs-target="#exampleModalCenteredScrollable"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="18"
+                                height="18"
+                              >
+                                <path fill="none" d="M0 0h24v24H0z" />
+                                <path
+                                  d="M6.414 16L16.556 5.858l-1.414-1.414L5 14.586V16h1.414zm.829 2H3v-4.243L14.435 2.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 18zM3 20h18v2H3v-2z"
+                                  fill="rgba(255,255,255,1)"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {isModalOpen && (
+                <Modal
+                  setIsModalOpen={setIsModalOpen}
+                  user={selectedUser}
+                  setList={setList}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://api.github.com/users`);
+
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
